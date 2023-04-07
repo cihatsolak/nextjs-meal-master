788050df8c1544484c000d2485baf32cfeedb03c
Cihat Solak <cihatsolak@hotmail.com>
2023-04-08 00:09:18 +0300

    feat(client): create search component

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,42 +1,54 @@
+import { useState } from "react";
 import Logo from "../ui/Logo";
 import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+import OutsideClickHandler from "react-outside-click-handler";
+import Title from "../ui/Title";
 
 export default function Header() {
+  const [isShowSearch, setIsShowSearch] = useState(true);
+
   return (
     <div className="h-[5.5rem] bg-secondary">
-    <div className="container mx-auto text-white flex justify-between items-center h-full">
-      <Logo />
-      <nav>
-        <ul className="flex gap-x-2">
-          <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-            <a href="">Home</a>
-          </li>
-          <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-            <a href="">Menu</a>
-          </li>
-          <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-            <a href="">About</a>
-          </li>
-          <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-            <a href="">Book Table</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="flex gap-x-4 items-center">
-        <a href="#">
-          <FaUserAlt />
-        </a>
-        <a href="#">
-          <FaShoppingCart />
-        </a>
-        <a href="#">
-          <FaSearch />
-        </a>
-        <a href="#">
-          <button className="btn-primary">Order Online</button>
-        </a>
+      <div className="container mx-auto text-white flex justify-between items-center h-full">
+        <Logo />
+        <nav>
+          <ul className="flex gap-x-2">
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Home</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Menu</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">About</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Book Table</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-x-4 items-center">
+          <a href="#">
+            <FaUserAlt className="hover:text-primary transition-all" />
+          </a>
+          <a href="#">
+            <FaShoppingCart className="hover:text-primary transition-all" />
+          </a>
+          <button onClick={() => setIsShowSearch(!isShowSearch)}>
+            <FaSearch className="hover:text-primary transition-all" />
+          </button>
+          <a href="#">
+            <button className="btn-primary">Order Online</button>
+          </a>
+        </div>
       </div>
+      {isShowSearch && (
+        <OutsideClickHandler onOutsideClick={() => setIsShowSearch(false)}>
+          <div className="title">
+            <Title className="text-9xl">Title</Title>
+          </div>
+        </OutsideClickHandler>
+      )}
     </div>
-  </div>
-  )
+  );
 }
