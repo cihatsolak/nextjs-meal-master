b18c7b55e758fca1e09981e25f6dcb3a82c820e8
Cihat Solak <cihatsolak@hotmail.com>
2023-04-09 13:46:58 +0300

    feat(client): add campaigns component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Carousel from '@/components/Carousel';
+import Campaigns from '@/components/Campaigns';
 
 export default function Index() {
   return (
-    <div className="relative">
+    <div>
       <Carousel />
+      <Campaigns/>
     </div>
   );
 }
