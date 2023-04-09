f18e0066812b2036509ddf5180f226fd2d81dc82
Cihat Solak <cihatsolak@hotmail.com>
2023-04-09 14:11:41 +0300

    feat(client): add about component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "@/components/Carousel";
 import Campaigns from "@/components/Campaigns";
 import MenuWrapper from "@/components/product/MenuWrapper";
+import About from "@/components/About";
 
 export default function Index() {
   return (
@@ -9,6 +10,7 @@ export default function Index() {
       <Carousel />
       <Campaigns />
       <MenuWrapper />
+      <About />
     </>
   );
 }
