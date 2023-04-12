f46f286beed2837b94534921be04aaa2669d31ef
Cihat Solak <cihatsolak@hotmail.com>
2023-04-12 20:15:44 +0300

    feat(client): login with github

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,16 +2,22 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/globals.css";
 import Layout from "@/layout/Layout";
+import { SessionProvider } from "next-auth/react";
 
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </SessionProvider>
   );
 }
