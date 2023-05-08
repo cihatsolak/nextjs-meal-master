2531d0b42e3ff0e39cba3cc02cb17e0c85517413
Cihat Solak <cihatsolak@hotmail.com>
2023-05-08 23:04:36 +0300

    feat(client): evaluate the overall structure

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,50 +1,66 @@
 import { useState } from "react";
-import Logo from "../ui/Logo";
 import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+import Logo from "../ui/Logo";
 import Search from "../ui/Search";
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useSelector } from "react-redux";
 
-export default function Header() {
-  const [isShowSearchModal, setIsShowSearchModal] = useState(false);
-  const [isShowMenuModal, setIsShowMenuModal] = useState(false);
-
+const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
   const cart = useSelector((state) => state.cart);
+
   const router = useRouter();
 
   return (
     <div
-      className={`h-[5.5rem] z-50 relative ${
+      className={`h-[5.5rem] z-50 fixed w-full ${
         router.asPath === "/" ? "bg-transparent" : "bg-secondary"
       }`}
     >
       <div className="container mx-auto text-white flex justify-between items-center h-full">
         <Logo />
         <nav
-          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${
-            isShowMenuModal === true && "!grid place-content-center"
+          className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden  ${
+            isMenuModal === true && "!grid place-content-center"
           }`}
         >
           <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
-            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+            <li
+              className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
+                router.asPath === "/" && "text-primary"
+              }`}
+            >
               <Link href="/">Home</Link>
             </li>
-            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+            <li
+              className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
+                router.asPath === "/menu" && "text-primary"
+              }`}
+            >
               <Link href="/menu">Menu</Link>
             </li>
-            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+            <li
+              className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
+                router.asPath === "/about" && "text-primary"
+              }`}
+            >
               <Link href="/about">About</Link>
             </li>
-            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+            <li
+              className={`px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer ${
+                router.asPath === "/reservation" && "text-primary"
+              }`}
+            >
               <Link href="/reservation">Book Table</Link>
             </li>
           </ul>
-          {isShowMenuModal && (
+          {isMenuModal && (
             <button
               className="absolute  top-4 right-4 z-50"
-              onClick={() => setIsShowMenuModal(false)}
+              onClick={() => setIsMenuModal(false)}
             >
               <GiCancel size={25} className=" transition-all" />
             </button>
@@ -53,34 +69,49 @@ export default function Header() {
         <div className="flex gap-x-4 items-center">
           <Link href="/auth/login">
             <span>
-              <FaUserAlt className="hover:text-primary transition-all cursor-pointer" />
+              <FaUserAlt
+                className={`hover:text-primary transition-all cursor-pointer ${
+                  (router.asPath.includes("profile") ||
+                    router.asPath.includes("auth")) &&
+                  "text-primary"
+                }`}
+                size={18}
+              />
             </span>
           </Link>
           <Link href="/cart">
             <span className="relative">
-              <FaShoppingCart className="hover:text-primary transition-all cursor-pointer" />
+              <FaShoppingCart
+                className={`hover:text-primary transition-all cursor-pointer ${
+                  router.asPath === "/cart" && "text-primary"
+                }`}
+                size={18}
+              />
               <span className="w-4 h-4 text-xs grid place-content-center rounded-full bg-primary absolute -top-2 -right-3 text-black font-bold">
                 {cart.products.length === 0 ? "0" : cart.products.length}
               </span>
             </span>
           </Link>
-          <button onClick={() => setIsShowSearchModal(!isShowSearchModal)}>
-            <FaSearch className="hover:text-primary transition-all cursor-pointer" />
+          <button onClick={() => setIsSearchModal(true)}>
+            <FaSearch
+              className="hover:text-primary transition-all cursor-pointer"
+              size={18}
+            />
           </button>
           <a href="#" className="md:inline-block hidden sm">
             <button className="btn-primary">Order Online</button>
           </a>
           <button
             className="sm:hidden inline-block"
-            onClick={() => setIsShowMenuModal(true)}
+            onClick={() => setIsMenuModal(true)}
           >
             <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
           </button>
         </div>
       </div>
-      {isShowSearchModal && (
-        <Search setIsShowSearchModal={setIsShowSearchModal} />
-      )}
+      {isSearchModal && <Search setIsSearchModal={setIsSearchModal} />}
     </div>
   );
-}
+};
+
+export default Header;
